6e690ad8e259ebed5a9874e31d50774ce3d35dae
gurkanguler <coder.gurkanguler16@gmail.com>
2020-05-24 21:36:49 +0300

    New

--- client.py
+++ client.py
@@ -1,67 +1,81 @@
-import socket,os,time,platform
+import socket,time,os,platform
 
 
-def MY_OS_CHECK():
+############# Istemci Fonk. #############
+def Client(host):
+    #################
+    HOST = host     
+    PORT = 42       
+    BUFF = 1024
+    ADDR = (HOST,PORT)     
+    #################
+
+    ######### Istemci Olustur ###############
+    main_client = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+
+    main_client.connect((HOST,PORT))
+    ######### Veri Gonder ################
+    send_data = str(input("[*] -> "))
+
+    ######### Komutu Gonder ###############
+    main_client.sendto(bytes(send_data,encoding="utf-8"),ADDR)
+
+    ########## VERI KONTROL ##############
+    if send_data == "PUT":
+        ########### Dosya Ismi ###################
+        file_name = str(input("[+] Dosya Ismi : ->"))
+        ########## Ismi Gonder ##############
+        main_client.sendto(bytes(file_name,encoding="utf-8"),ADDR)
+        ####### PUT Fonk. Cagir ############
+        PUT(main_client,file_name,HOST,PORT,ADDR)
+    
+    if send_data == "GET":
+        ######### DOSYA ISMI AL ##########
+        file_name = str(input("[*] Dosya Ismi -> "))
+        ############ ISMI GONDER ################
+        main_client.sendto(bytes(file_name,encoding="utf-8"),ADDR)
+        ######### GET FONK. CAGIR #############
+        GET(file_name)
+
+
+############## PUT ################
+
+def PUT(main_client,file_name,HOST,PORT,ADDR):
+
+    ######### Dosyayi AC #################
+    open_file = open(file_name,"rb")
+
+    ######### Dosyayı Oku ##################
+    for read_file in open_file:
+        ########## Dosyayı GONDER ##############
+        main_client.sendto(read_file,ADDR)
+
+    print("[*] Dosya Gonderildi...\n")
+
+
+################ GET ####################
+
+def GET(open_file):
+    HOST = "0.0.0.0"
+    PORT = 42
+    BUFF = 1024
+    new_server = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+    new_server.bind((HOST,PORT))
+    txt,addr = new_server.recvfrom(BUFF)
+    create_file = open(open_file,"wb")
+    new_txt = txt.decode("utf-8").strip()
+    for write_file in new_txt:
+        create_file.write(bytes(str(write_file),encoding="utf-8"))
+
+
+######## CALISTIR ###################
+if __name__ == "__main__":
+    ######## Isletim sistemi kontrolu ########
     os_inf = platform.system()
     if os_inf == "Linux":
         os.system("clear")
     elif os_inf == "Windows":
         os.system("cls")
-
-
-PORT = 42
-
-def Client(HOST):
-
-    BUF = 1024
-
-    ADDR = (HOST,PORT)
-
-    sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-    
-    sock.connect((HOST,PORT))
-
-    User_Command(sock,HOST,ADDR,BUF)
-
-
-def User_Command(sock,HOST,ADDR,BUF):
-    user_command = str(input(" [+] -> "))
-    sock.sendto(bytes(user_command,encoding="utf-8"),ADDR)
-    if user_command == "PUT":
-        PUT(sock,HOST,ADDR)
-    elif user_command == "GET":
-        GET(sock)
-    elif user_command == "LIST":
-        LIST(HOST,ADDR)
-
-
-def PUT(sock,HOST,ADDR):
-    file_name = str(input(" [+] Enter to file name -> "))
-    print(" [*] File Name : ",file_name)
-    sock.sendto(bytes(file_name,encoding="utf-8"),ADDR)
-    read_file = open(file_name,"rb")
-    for read_file_line in read_file:
-        sock.sendto(read_file_line,ADDR)
-
-def GET(sock):
-	sock.close()
-	Reverse_Client()
-
-def Reverse_Client():
-	HOST = "0.0.0.0"
-	PORT = 42
-	BUFF = 1024
-	sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-	sock.bind((HOST,PORT))
-	server_data, addr = sock.recvfrom(BUFF)
-	new_data = server_data.decode("utf-8").strip()
-	for print_data in new_data:
-		print(print_data)
-
-
-if __name__ == "__main__":
-    MY_OS_CHECK()
-    print("CLIENT".center(40,'-'))
-    host = str(input(" [+] Ip Address : -> "))
+    print("Istemci\n")
+    host = str(input("[*] Ip Adresini Giriniz -> "))
     Client(host)
-
--- server.py
+++ server.py
@@ -1,63 +1,81 @@
-import socket,os,platform,time
-import subprocess
-import pickle
+import socket,time,os
+import platform
 
-def MY_OS_CHECK():
+
+
+def Main_Server():
+    #################
+    HOST = "0.0.0.0"#
+    PORT = 42       #
+    BUFF = 1024     #
+    #################
+
+    ######## SERVER OLUSTUR ############
+    main_server = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+
+    ######## SERVER DINLE ###############
+    main_server.bind((HOST,PORT))
+
+    ######## Istemci Komut Kontrol ################
+    client_command,address = main_server.recvfrom(BUFF)
+    
+    client_command = client_command.decode("utf-8").strip()
+    
+    if client_command == "PUT":
+        ############ PUT Fonk Cagir ############
+        PUT(main_server,HOST,PORT,BUFF)
+    if client_command == "GET":
+        ############ DOSYA ISMINI AL #################
+        file_name,address = main_server.recvfrom(BUFF)
+        ############ GET FONK. CAGIR #################
+        client_addr = address[0]
+
+        GET(file_name,client_addr)
+
+
+
+def GET(file_name,client_addr):
+    file_open = open(file_name,"rb")
+    for file_read in file_open:
+        print(file_read)
+    print(client_addr)
+    New_Socket(client_addr,42,file_read)
+
+
+
+def New_Socket(client_host,port,file_name):
+    new_socket = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+    new_socket.connect((client_host,port))
+    file_send = new_socket.sendto(file_name,(client_host,port))
+       
+
+
+def PUT(main_server,HOST,PORT,BUFF):
+    ######### Dosya Adını Al ###############
+    file_name,address = main_server.recvfrom(BUFF)
+    ########### Dosyayı Olustur #############
+    create_file = open(file_name,"wb")
+    ############ Dosyayı Yaz ##############
+    file_txt,address = main_server.recvfrom(BUFF)
+    new_file_txt = file_txt.decode("utf-8").strip()
+    for write_file in new_file_txt:
+        create_file.write(bytes(str(write_file),encoding="utf-8"))
+
+
+
+
+   
+
+
+
+
+############# CALISTIR ################
+if __name__ == "__main__":
+    ########### Isletim Sistemi Kontrolu ###########
     os_inf = platform.system()
     if os_inf == "Linux":
         os.system("clear")
     elif os_inf == "Windows":
         os.system("cls")
-
-
-
-
-def Server():
-    HOST = "0.0.0.0"
-    PORT = 42
-    BUF = 1024
-    ADDR = (HOST,PORT)
-    sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-    result = sock.bind((HOST,PORT))
-    if result == None:
-        print("\n")
-        print(" [*] Server Started...")
-        user_command,address = sock.recvfrom(BUF)
-        user_command = user_command.decode("utf-8").strip()
-        if user_command == "PUT":
-            file_name,address = sock.recvfrom(BUF)
-            file_name = file_name.decode("utf-8").strip()
-            print(" [*] File Name : ",file_name)
-            file_create = open(file_name,"wb")
-            file_txt, address = sock.recvfrom(BUF)
-            file_txt_new = file_txt.decode("utf-8").strip()
-            for file_write in file_txt_new:
-                file_create.write(bytes(file_write,encoding="utf-8"))
-        if user_command == "GET":
-        	sock.close()
-        	Reverse_Server(address)
-        	
-
-
-
-    else:
-        print(" [*] Server Error...")
-
-def Reverse_Server(host):
-	HOST = host[0]
-	PORT = 42
-	ADDR = (HOST,PORT)
-	sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-	sock.connect((HOST,PORT))
-	data = subprocess.check_output(["ls"]).decode("utf-8").split("\n")
-	print(len(data))
-	for list_data in data:
-		sock.sendto(bytes(list_data,encoding="utf-8"),ADDR)
-	
-
-
-
-if __name__ == "__main__":
-    MY_OS_CHECK()
-    print("Server".center(40,'-'))
-    Server()
+    print('SERVER')
+    Main_Server()
